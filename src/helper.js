252a0c3f5bb2f3d9cf80a2bfb5d7d735c8e3f9bd
sachin <sangwansachin631@gmail.com>
2023-09-28 19:40:55 +0530

    as

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
 // export const Base_url="http://localhost:8080"
-export const Base_url="https://doctorappbackend-8zm3.onrender.com"
+export const Base_url="https://adorable-baklava-9864f4.netlify.app"
